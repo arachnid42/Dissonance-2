d5947033fbba729de6878b0fc42077fc25e94edd
Alexander <dubbinary@gmail.com>
2018-07-21 20:38:16 +0300

    Added new themes; Fix Time counter bug

--- Assets/Scripts/UI/Elements/ThemeButton.cs
+++ Assets/Scripts/UI/Elements/ThemeButton.cs
@@ -25,6 +25,11 @@ namespace Assets.Scripts.UI.Elements
         private const float OFFSET_SELECTED = 10;
         private const float OFFSET_UNSELECTED = 5;
 
+        private const float BADGE_UNSELECTED_X = 73;
+        private const float BADGE_UNSELECTED_Y = 216.8f;
+        private const float BADGE_SELECTED_X = 68;
+        private const float BADGE_SELECTED_Y = 211.8f;
+
         public void Setup(bool isFree, bool isSelected, Sprite background, UnityAction action)
         {
             SetupBadge(isFree);
@@ -50,15 +55,22 @@ namespace Assets.Scripts.UI.Elements
         public void SetSelected(bool isSelected)
         {
             button.interactable = !isSelected;
+            RectTransform badge = badgeImage.GetComponent<RectTransform>();
+            Debug.LogFormat("offsetMin: {0} offsetMax: {1}",badge.offsetMin,badge.offsetMax);
+            //Debug.LogFormat("rect: {0} position: {1}", badge.rect,badge.position);
             if (isSelected)
             {
                 backgroundImage.GetComponent<RectTransform>().offsetMin = new Vector2(OFFSET_SELECTED, OFFSET_SELECTED);
                 backgroundImage.GetComponent<RectTransform>().offsetMax = new Vector2(-OFFSET_SELECTED, -OFFSET_SELECTED);
+                badgeImage.GetComponent<RectTransform>().offsetMin = new Vector2(BADGE_SELECTED_X, BADGE_SELECTED_Y);
+
             }
             else
             {
                 backgroundImage.GetComponent<RectTransform>().offsetMin = new Vector2(OFFSET_UNSELECTED, OFFSET_UNSELECTED);
                 backgroundImage.GetComponent<RectTransform>().offsetMax = new Vector2(-OFFSET_UNSELECTED, -OFFSET_UNSELECTED);
+                badgeImage.GetComponent<RectTransform>().offsetMin = new Vector2(BADGE_UNSELECTED_X, BADGE_UNSELECTED_Y);
+
             }
         }
     }
