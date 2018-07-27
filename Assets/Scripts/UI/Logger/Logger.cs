72e5332a305b708a522626dcbdcc307b94b7d3aa
Alexander <dubbinary@gmail.com>
2018-07-28 00:36:17 +0300

    added simple logger

---
+++ Assets/Scripts/UI/Logger/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace Assets.Scripts.UI.Logger
+{
+    public class Logger : MonoBehaviour
+    {
+        [SerializeField]
+        public Text text;
+
+        private void OnEnable()
+        {
+            Application.logMessageReceived += HandleLog;
+        }
+        private void OnDisable()
+        {
+            Application.logMessageReceived -= HandleLog;
+        }
+        private void HandleLog(string logString, string stackTrace, LogType type)
+        {
+            text.text = text.text + "\n" + logString;
+        }
+
+        public void OnClearButtonClick()
+        {
+            text.text = "";
+        }
+    }
+}
+
